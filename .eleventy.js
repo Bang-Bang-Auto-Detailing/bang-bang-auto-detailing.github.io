696fdae73d59afa3cd590b5ca6162543f3983e3c
Albert Volkman <albert.volkman@gmail.com>
2025-07-22 14:06:27 -0400

    Make web3form key a global variable

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ export default function (eleventyConfig) {
   eleventyConfig.addGlobalData('site_title', 'Bang Bang Auto Detailing');
   eleventyConfig.addGlobalData('email', 'bangbangautodetailing@gmail.com');
   eleventyConfig.addGlobalData('phone', '(407) 878-9967');
+  eleventyConfig.addGlobalData('form_key', '00cdba78-03cc-46a9-8487-28d898a0945d');
   eleventyConfig.setIncludesDirectory('_includes');
   eleventyConfig.setLayoutsDirectory('_layouts');
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
