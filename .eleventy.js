2d5f418def162a01f2f61895765cd60a6bb24fb5
Albert Volkman <albert.volkman@gmail.com>
2025-07-22 14:06:54 -0400

    Update form key to Roubens

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ export default function (eleventyConfig) {
   eleventyConfig.addGlobalData('site_title', 'Bang Bang Auto Detailing');
   eleventyConfig.addGlobalData('email', 'bangbangautodetailing@gmail.com');
   eleventyConfig.addGlobalData('phone', '(407) 878-9967');
-  eleventyConfig.addGlobalData('form_key', '00cdba78-03cc-46a9-8487-28d898a0945d');
+  eleventyConfig.addGlobalData('form_key', '22014556-8e60-44ea-a7d8-ebf8afea0a6d');
   eleventyConfig.setIncludesDirectory('_includes');
   eleventyConfig.setLayoutsDirectory('_layouts');
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
