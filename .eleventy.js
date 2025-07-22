8f039fc77b7ade5e47d039bab3ed60e07b1b9ac9
Albert Volkman <albert.volkman@gmail.com>
2025-07-22 13:44:48 -0400

    Replace phone/email with variables

--- .eleventy.js
+++ .eleventy.js
@@ -3,11 +3,10 @@ import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     'assets': 'assets',
-    'css': 'css',
-    'js': 'js',
-    'uploads': 'uploads',
   });
   eleventyConfig.addGlobalData('site_title', 'Bang Bang Auto Detailing');
+  eleventyConfig.addGlobalData('email', 'bangbangautodetailing@gmail.com');
+  eleventyConfig.addGlobalData('phone', '(407) 878-9967');
   eleventyConfig.setIncludesDirectory('_includes');
   eleventyConfig.setLayoutsDirectory('_layouts');
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
